3cfe899943b043f7e8a8096ea7ecef9cc55ff287
Renato Junior <levandeoficial@gmail.com>
2020-08-18 23:19:08 -0300

    Projeto

--- src/main.c
+++ src/main.c
@@ -1,4 +1,64 @@
-// Test
-//Test2
-//test3
-//teste4 teste 98
+#include <stdio.h>
+int main() {
+    /*
+        Parte 1 - Ler variáveis de um arquivo
+    */
+
+
+    /*
+        Parte 2 - Montar uma matriz a partir do arquivo
+    */
+
+
+    /*
+        Parte 3 - Fazer cálculo do passo 5.
+    */
+
+
+    /*
+        Parte 4 - Fazer cálculo do passo 6.
+    */
+
+
+    /*
+        Parte 5 - Fazer cálculo do passo 7.
+    */
+
+
+    /*
+        Parte 6 - Fazer cálculo do passo 8.
+    */
+
+
+    /*
+        Parte 7 - Fazer cálculo do passo 9.
+    */
+
+
+    /*
+        Parte 7 - Fazer cálculo do passo 10.
+    */
+
+
+    /*
+        Parte 8 - Fazer cálculo do passo 11.
+    */
+
+
+    /*
+        Parte 9 - Fazer cálculo do passo 12.
+    */
+
+
+    /*
+        Parte 10 - Fazer cálculo do passo 13.
+    */
+
+
+    /*
+        Parte 11 -Exibir o resultado - passo 14.
+    */
+
+
+    return 0;
+}

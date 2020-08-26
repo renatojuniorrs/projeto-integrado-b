45d254de94736538965499116dd678538f6d9560
Guilherme Araujo Alexandre <guilhermealexandre167@gmail.com>
2020-08-25 23:11:37 -0300

    Programação 25/08
    Co-Authored-By: Renato Junior <renatojuniorrs@users.noreply.github.com>
    Co-Authored-By: null <lucasamt@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -3,15 +3,43 @@
 #include <locale.h>
 #include <conio.h>
 
-int main() {
+int main()
+{
+    setlocale(LC_ALL,"portuguese");
     /*
         Parte 1 - Ler variáveis de um arquivo
         - Ler arquivo
     */
+
+    /*
+    Parte 2 - Montar uma matriz a partir do arquivo
+    */
+
+    // Perguntar ao usuario qual o tamanho da matriz? (n)
+    // Ler o tamnho da matriz (n)
+    int tamanho_matriz;
+     printf("Qual o tamanho da matriz?");
+     scanf("%d", &tamanho_matriz);
+
+    // Criar matriz com a quantidade n linhas e (n+1) colunas
+
+
+
+    // Perguntar ao usuario qual é o arquivo que ele vai ler
+    // Tentar ler o arquivo, se não conseguir ler o arquivo, dar uma mensagem de erro
+    // Se conseguir ler o arquivo, ler linha a linha do arquivo e colocar na matriz
+
     FILE *file;
     file = fopen ("ProjetoIntB.txt","w");
-    fprint(file,"");
-    // AQUI
+
+    float matriz[3][4];
+
+    fscanf(file, "%i %i %i", &matriz[0][0], &matriz[0][1], &matriz[0][2], &matriz[0][3]);
+
+    printf("%i %i %i", matriz[0][0], matriz[0][1], matriz[0][2], matriz[0][3]);
+
+    fclose(file);
+
     // Essa parte sai - inicio
 
     float matriz[3][4] =  {
@@ -26,22 +54,73 @@ int main() {
         },
     };
 
-    // Essa parte sai - fim
 
-    /*
-        Parte 2 - Montar uma matriz a partir do arquivo
-    */
-    
+    // Essa parte sai - fim
 
 
     /*
         Parte 3 - Fazer cálculo do passo 5.
     */
+    int quantidade_de_colunas = tamanho_matriz-1;
+    int quantidade_de_linhas = tamanho_matriz; // Não esquecer de subtrair
+    int cont = 0;
+
+    for (int i = 0; i < quantidade_de_linhas; i++)
+    {
+       //matriz[0][0]/matriz[1][0]
+       //matriz[0][1]/matriz[1][1]
+       //matriz[0][2]//matriz[2][2]
+
+        //PRIMEIRO par de linhas
+        if( (matriz[quantidade_de_colunas][i]/matriz[(quantidade_de_colunas+1)][i]) != (matriz[quantidade_de_colunas][(i+1)]/matriz[(quantidade_de_colunas+1)][(i+1)]) // 0/4 != 3/0
+        && (matriz[q])
+        {
+
+        }
+            /* code */
+    }
 
 
     /*
         Parte 4 - Fazer cálculo do passo 6.
     */
+    // Não pode ter 0 na diagonal principal
+
+    int primeiro_nao_zero;
+
+    for (int i = 0; i < quantidade_de_linhas; i++)
+    {
+        if(matriz[i][i] == 0)
+        {
+            primeiro_nao_zero = 0;
+            // Pegar primeiro não zero da linha
+            for (int j = 0; j < quantidade_de_linhas; j++)
+            {
+                if(matriz[i][j] !=  0)
+                {
+                    primeiro_nao_zero = matriz[i][j];
+                    break;
+                }
+            }
+
+            if(primeiro_nao_zero == 0)
+            {
+                operacao_impossivel();
+            }
+            else
+            {
+                // Se tiver um número válido, vamos trocar
+                for (int j = 0; j < quantidade_de_linhas; j++)
+                {
+                    if(i == j)
+                    {
+                        matriz[i][j] = primeiro_nao_zero;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
 
     /*
@@ -86,3 +165,15 @@ int main() {
 
     return 0;
 }
+
+int operacao_impossivel(){
+    system("clear");
+    printf("- Não é possível concluir esta operação!");
+    system("pause");
+
+    return 0;
+}
+
+// aqui
+}
+double** nova

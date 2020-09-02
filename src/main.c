e0ea479ec227523bd50685317a05c79701fd18c1
Renato Junior <levandeoficial@gmail.com>
2020-09-01 23:07:20 -0300

    Projeto em Aula
    Co-Authored-By: null <Guilherme-A-A@users.noreply.github.com>
    Co-Authored-By: Lucas Ament <lucasamt@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -18,260 +18,263 @@ int main()
     Parte 2 - Montar uma matriz a partir do arquivo
     */
 
-    // Perguntar ao usuario qual o tamanho da matriz? (n)
     // Ler o tamnho da matriz (n)
-    int tamanho_matriz = 50;
-    // printf("Qual o tamanho da matriz?\n");
-    // scanf("%d", &tamanho_matriz);
+
+    FILE *fp;
+    unsigned int tamanho_matriz;
+    char nome_arquivo[1024], line[1024];
+    printf("Digite o nome do arquivo que deseja ler: \n");
+    scanf("%s", &nome_arquivo);
+
+    system("cls");
+
+    printf("-- Estamos tentando abrir o arquivo: %s\n", nome_arquivo);
+    fp = fopen(nome_arquivo, "rb");
+    if (fp)
+    {
+        fscanf(fp, "%u", &tamanho_matriz);
+    }
+    else
+    {
+        printf("-- Não conseguimos abrir o arquivo: %s :P\n", nome_arquivo);
+        system("pause");
+    }
+    fclose(fp);
 
     // Criar matriz com a quantidade n linhas e (n+1) colunas
     double** matriz = novamatriz(tamanho_matriz, (tamanho_matriz+1));
+    carregaMatriz(nome_arquivo, matriz);
 
-    // Perguntar ao usuario qual é o arquivo que ele vai ler
-    char  arquivo[1024]; // String
-    matriz = ler_arquivo(matriz);
-    print_f("%d", matriz[0][0]);
-    system("pause");
-//
-//     // Tentar ler o arquivo, se não conseguir ler o arquivo, dar uma mensagem de erro
-//
-//
-//     // Se conseguir ler o arquivo, ler linha a linha do arquivo e colocar na matriz
-//
-//
-//     FILE *file;
-//     file = fopen ("ProjetoIntB.txt","w");
-//
-//     float matriz[3][4];
-//
-//     fscanf(file, "%i %i %i", &matriz[0][0], &matriz[0][1], &matriz[0][2], &matriz[0][3]);
-//
-//     printf("%i %i %i", matriz[0][0], matriz[0][1], matriz[0][2], matriz[0][3]);
-//
-//     fclose(file);
-//
-//     // Essa parte sai - inicio
-//
-//     float matriz[3][4] =  {
-//         {
-//             0,3,2,28
-//         },
-//         {
-//             4,0,2,24
-//         },
-//         {
-//             2,3,0,16
-//         },
-//     };
-//
-//
-//     // Essa parte sai - fim
-//
-//
-//     /*
-//     Parte 3 - Fazer cálculo do passo 5.
-//     */
-//     int quantidade_de_colunas = tamanho_matriz-1;
-//     int quantidade_de_linhas = tamanho_matriz; // Não esquecer de subtrair
-//     int cont = 0, c;
-//     float divisao_linha, incremento_linha;
-//
-//     // Ver se a divisão das linhas não seja igual para continuar o cálculo
-//     for (int i = 0; i < quantidade_de_linhas; i++)
-//     {
-//         for(c = 0; c < quantidade_de_linhas; c++)
-//         {
-//             for(j = 0; j <= quantidade_de_linhas; j++)
-//             {
-//                 if( i != c && c > i)
-//                 {
-//                     if(j == 0)
-//                     {
-//                         divisao_linha = matriz[i][j] / matriz[c][j];
-//                         incremento_linha = divisao_linha;
-//                     }
-//                     incremento_linha = matriz[i][j] / matriz[c][j];
-//                     if(divisao_linha == incremento_linha)
-//                     {
-//                         cont++;
-//                     }
-//                 }
-//             }
-//             if(cont == quantidade_de_linhas + 1)
-//             {
-//                 operacao_impossivel();
-//             }
-//             else
-//             {
-//                 count = 0;
-//             }
-//         }
-//     }
-// }
-//
-// // Verificação de valores corretos - INICIO
-// if(matriz[2][2] != 2){
-//     operacao_impossivel();
-// }
-// // Verificação de valores corretos - FIM
-//
-// /*
-// Parte 4 - Fazer cálculo do passo 6.
-// */
-// // Não pode ter 0 na diagonal principal
-//
-// int primeiro_nao_zero;
-//
-// for (int i = 0; i < quantidade_de_linhas; i++)
-// {
-//     if(matriz[i][i] == 0)
-//     {
-//         primeiro_nao_zero = 0;
-//         // Pegar primeiro não zero da linha
-//         for (int j = 0; j < quantidade_de_linhas; j++)
-//         {
-//             if(matriz[i][j] !=  0)
-//             {
-//                 primeiro_nao_zero = matriz[i][j];
-//                 break;
-//             }
-//         }
-//
-//         if(primeiro_nao_zero == 0)
-//         {
-//             operacao_impossivel();
-//         }
-//         else
-//         {
-//             // Se tiver um número válido, vamos trocar
-//             for (int j = 0; j < quantidade_de_linhas; j++)
-//             {
-//                 if(i == j)
-//                 {
-//                     matriz[i][j] = primeiro_nao_zero;
-//                     break;
-//                 }
-//                 else if(primeiro_nao_zero != 0 && primeiro_nao_zero == matriz[i][j])
-//                 {
-//                     matriz[i][j] = 0;
-//                     primeiro_nao_zero = 0;
-//                 }
-//             }
-//         }
-//     }
-// }
-// /*
-// Parte 5 - Fazer cálculo do passo 7.
-// */
-// int numdivisor_linha = matriz[0][0];
-// float divisao1linha_linha;
-//
-// for (j = 0; j < quantidade_de_colunas; j++)
-// {
-//     matriz[0][0] = 1;
-//     divisao1linha_linha = matriz[0][j] / numdivisor_linha;
-// }
-// /*
-// Parte 6 - Fazer cálculo do passo 8.
-// */
-// float numeroparazerar_coluna = - matriz[i][0], zerar_coluna;
-//
-// for(i = 1; i < quantidade_de_linhas; i++)
-// {
-//     zerar_coluna = matriz[i][0] * numeroparazerar_coluna;
-// }
-// /*
-// Parte 7 - Fazer cálculo do passo 9.
-//*/
-/*
-// // Nao pode ter 0 na diagonal principal
 
-int segundo_nao_zero;
+    /*
+    Parte 3 - Fazer cálculo do passo 5.
+    */
+    int quantidade_de_colunas = tamanho_matriz-1;
+    int quantidade_de_linhas = tamanho_matriz; // Não esquecer de subtrair
+    int cont = 0, c;
+    float divisao_linha, incremento_linha;
 
-for (int i=0; i < quantidade_de_linhas; i++);
-{
-      if(matriz[i][i] == 0);
-      {
-       segundo_nao_zero = 0;
-       //Pegar segundo nao zero da linha
-       for (int j = 0; j < quantidade_de_linhas; j++);
-       {
-           if(matriz[i][j] != 0);
-           {
-            segundo_nao_zero = matriz[i][j];
-            break;
-         }
-     }
-
-      if(segundo_nao_zero == 0);
-      {
-          operacao_impossivel();
-    }
-    else
-{
-           //Se tiver um numero valido vamos trocar
-          for (int j= 0; j < quantidade_de_linhas; j++);
-  {
-      if(i == j)
-      {
-              matriz[i][j] = segundo_nao_zero;
-              break
+    // Ver se a divisão das linhas não seja igual para continuar o cálculo
+    for (int i = 0; i < quantidade_de_linhas; i++)
+    {
+        for(int c = 0; c < quantidade_de_linhas; c++)
+        {
+            for(int j = 0; j <= quantidade_de_linhas; j++)
+            {
+                if( i != c && c > i)
+                {
+                    if(j == 0)
+                    {
+                        divisao_linha = matriz[i][j] / matriz[c][j];
+                        incremento_linha = divisao_linha;
+                    }
+                    incremento_linha = matriz[i][j] / matriz[c][j];
+                    if(divisao_linha == incremento_linha)
+                    {
+                        cont++;
+                    }
+                }
             }
-            else if(segundo_nao_zero != 0 && segundo_nao_zero == matriz[i][j])
-
-      }
-
-  }
+            if(cont == quantidade_de_linhas + 1)
+            {
+                operacao_impossivel();
+            }
+            else
+            {
+                cont = 0;
+            }
+        }
     }
-}
 
 
-*/
-//                 {
-//                     matriz[i][j] = primeiro_nao_zero;
-//                     break;
-//                 }
-//                 else if(primeiro_nao_zero != 0 && primeiro_nao_zero == matriz[i][j])
-//                 {
-//                     matriz[i][j] = 0;
-//                     primeiro_nao_zero = 0;
-//                 }
-//             }
-//         }
-//     }
-// }
-//  {
-//  printf("Exibindo os valores da matriz \n\n");
-//printf("matriz[%d] = %d \n", i, matriz[]);
-//}
 
 
-}
-/*
-// /*
-// Parte 7 - Fazer cálculo do passo 10.
-// */
-//
-//
-// /*
-// Parte 8 - Fazer cálculo do passo 11.
-// */
-//
-//
-// /*
-// Parte 9 - Fazer cálculo do passo 12.
-// */
-//
-//
-// /*
-// Parte 10 - Fazer cálculo do passo 13.
-// */
-//
-//
-// /*
-// Parte 11 -Exibir o resultado - passo 14.
-// */
+    /*
+    Parte 4 - Fazer cálculo do passo 6.
+    */
+    // Não pode ter 0 na diagonal principal
+
+    double primeiro_nao_zero;
+
+    for (int i = 0; i < quantidade_de_linhas; i++)
+    {
+        if(matriz[i][i] == 0.0)
+        {
+            // Pegar primeiro não zero da linha
+            primeiro_nao_zero = 0.0;
+            for (int j = 0; j < quantidade_de_linhas; j++)
+            {
+                if(matriz[i][j] != 0.0)
+                {
+                    primeiro_nao_zero = matriz[i][j];
+                    break;
+                }
+            }
+
+            printf("--->> %f\n", primeiro_nao_zero);
+            system("pause");
+            
+            if(primeiro_nao_zero == 0.0)
+            {
+                operacao_impossivel();
+            }
+            else
+            {
+                // Se tiver um número válido, vamos trocar
+                for (int j = 0; j < quantidade_de_linhas; j++)
+                {
+                    if(i == j)
+                    {
+                        matriz[i][j] = primeiro_nao_zero;
+                        break;
+                    }
+                    else if(primeiro_nao_zero != 0.0 && primeiro_nao_zero == matriz[i][j])
+                    {
+                        matriz[i][j] = 0.0;
+                        primeiro_nao_zero = 0.0;
+                    }
+                }
+            }
+        }
+    }
+
+    // Verificação de valores corretos - INICIO
+    for (int a = 0; a < tamanho_matriz; a++) {
+        for (int b = 0; b < (tamanho_matriz+1); b++) {
+            printf(" [%d][%d] = %f\n", a, b, matriz[a][b]);
+        }
+    }
+    // Verificação de valores corretos - FIM
+
+    // /*
+    // Parte 5 - Fazer cálculo do passo 7.
+    // */
+    // int numdivisor_linha = matriz[0][0];
+    // float divisao1linha_linha;
+    //
+    // for (j = 0; j < quantidade_de_colunas; j++)
+    // {
+    //     matriz[0][0] = 1;
+    //     divisao1linha_linha = matriz[0][j] / numdivisor_linha;
+    // }
+    // /*
+    // Parte 6 - Fazer cálculo do passo 8.
+    // */
+    // float numeroparazerar_coluna = - matriz[i][0], zerar_coluna;
+    //
+    // for(i = 1; i < quantidade_de_linhas; i++)
+    // {
+    //     zerar_coluna = matriz[i][0] * numeroparazerar_coluna;
+    // }
+    // /*
+    // Parte 7 - Fazer cálculo do passo 9.
+    //*/
+    // // Nao pode ter 0 na diagonal principal
+    //
+    // int segundo_nao_zero;
+    //
+    // for (int i=0; i < quantidade_de_linhas; i++);
+    // {
+    //      if(matriz[i][i] == 0);
+    //      {
+    //       segundo_nao_zero = 0;
+    //       //Pegar segundo nao zero da linha
+    //       for (int j = 0; j < quantidade_de_linhas; j++);
+    //       {
+    //           if(matriz[i][j] != 0);
+    //           {
+    //                 segundo_nao_zero = matriz[i][j];
+    //                 break;
+    //         }
+    //     }
+    //
+    //        if(segundo_nao_zero == 0);
+    //        {
+    //           operacao_impossivel();
+    //        }
+    //        else
+    //        {
+    //             //Se tiver um numero valido vamos trocar
+    //            for (int j= 0; j < quantidade_de_linhas; j++);
+    //            {
+    //                 if(i == j)
+    //                 {
+    //                      matriz[i][j] = segundo_nao_zero;
+    //                      break
+    //                 }
+    //                 else if(segundo_nao_zero != 0 && segundo_nao_zero == matriz[i][j])
+    //                 {
+    //                    matriz [i][j] = 0;
+    //                    segundo_nao_zero = 0;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // /*
+    // Parte 7 - Fazer cálculo do passo 10.
+    // */
+    // // Não pode ter 0 na diagonal principal
+    //
+    // int segundo_nao_zero;
+    //
+    // for(int i=0; i < quantidade_de_colunas; i++);
+    // {
+    //       if(matriz[i][i] == 0);
+    //       {
+    //       segundo_nao_zero = 0;
+    //       //Pegar segundo nao zero da linha
+    //       for (int j = 0; j < quantidade_de_colunas; j++);
+    //       {
+    //           if (matriz[i][j] != 0);
+    //           {
+    //                  segundo_nao_zero = matriz[i][j];
+    //                  break;
+    //          }
+    //       }
+    //
+    //        if(segundo_nao_zero == 0);
+    //        {
+    //            operacao_impossivel();
+    //        }
+    //        else
+    //        {
+    //              //Se tiver um numero valido vamos trocar
+    //              for (int j = 0; j < quantidade_de_colunas; j++);
+    //              {
+    //                   if(i == j)
+    //                   {
+    //                         matriz[i][j] =segundo_nao_zero;
+    //                         break
+    //                   }
+    //                   else if(segundo_nao_zero != 0 && segundo_nao_zero == matriz[i][j]);
+    //                   {
+    //                      matriz [i][j] = 0;
+    //                      segundo_nao_zero = 0;
+    //                   }
+    //              }
+    //          }
+    //      }
+    //  }
+    // /*
+    // Parte 8 - Fazer cálculo do passo 11.
+    // */
+    //
+    //
+    // /*
+    // Parte 9 - Fazer cálculo do passo 12.
+    // */
+    //
+    //
+    // /*
+    // Parte 10 - Fazer cálculo do passo 13.
+    // */
+    //
+    //
+    // /*
+    // Parte 11 -Exibir o resultado - passo 14.
+    // */
 
 
-return 0;
+    return 0;
 }
--- src/mods/functions.c
+++ src/mods/functions.c
@@ -6,37 +6,26 @@ int operacao_impossivel(){
     return 0;
 }
 
-double* str_split(char* a_str, const char a_delim)
+// char* limpar_string(char* st){
+//     for (i = 0; i < strlen(st); i++) {
+//         a_str[i] =
+//     }
+// }
+
+void carregaMatriz(char* nomeArq, double** mat)
 {
-    char piece[1024] = "";
-    size_t i, arr_size = 0;
+    unsigned int qtdLins, lin, qtdCols, col;
 
-    for (i = 0; i < strlen(a_str); i++) {
-        if(a_str[i] == " "){
-            piece = "";
-            arr_size++;
+    FILE* arq = fopen(nomeArq, "r");
+
+    fscanf(arq, "%u", &qtdLins);
+    qtdCols = qtdLins+1;
+
+    for (lin = 0; lin < qtdLins; lin++)
+        for (col = 0; col < qtdCols; col++)
+        {
+            fscanf(arq, "%lf", &mat[lin][col]);
         }
-    }
-    arr_size++;
-
-    double* matriz;
-    double* matriz = (double* )malloc(arr_size*sizeof(double));
-
-    arr_size = 0;
-
-    for (i = 0; i < strlen(a_str); i++) {
-        if(a_str[i] == " "){
-            sscanf(piece, "%lf", &matriz[arr_size]);
-            arr_size++;
-            piece = "";
-        }else{
-            piece = piece + a_str[i];
-            arr_size++;
-        }
-    }
-    sscanf(piece, "%lf", &matriz[arr_size]);
-
-    return matriz;
 }
 
 double** novamatriz (unsigned int quantidade_de_linhas, unsigned int quantidade_de_colunas)
@@ -49,39 +38,4 @@ double** novamatriz (unsigned int quantidade_de_linhas, unsigned int quantidade_
     ret[linha] = (double*)malloc(quantidade_de_colunas*sizeof(double));
 
     return ret;
-}
-
-double** ler_arquivo(double** matriz)
-{
-    double* tokens;
-    FILE *fp;
-    int line_m = 0, column_m = 0;
-    char nome_arquivo[1024], line[1024];
-    printf("Digite o nome do arquivo que deseja ler: \n");
-    scanf("%s", &nome_arquivo);
-    // system("cls");
-    printf("-- Estamos tentando abrir o arquivo: %s\n", nome_arquivo);
-    fp = fopen(nome_arquivo, "rb");
-    if (fp)
-    {
-        while (fgets(line, sizeof(line), fp)) {
-            tokens = str_split(line, " ");
-            if (tokens)
-            {
-                for (size_t i = 0; i < (sizeof(tokens) / sizeof(double)); i++) {
-                    matriz[line_m][column_m] = tokens[i];
-                    column_m++;
-                }
-            }
-            line_m++;
-        }
-    }
-    else
-    {
-        printf("-- Não conseguimos abrir o arquivo: %s :P\n", nome_arquivo);
-        system("pause");
-    }
-    fclose(fp);
-
-    return matriz;
 }
